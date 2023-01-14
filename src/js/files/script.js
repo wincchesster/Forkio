0abec087db72a93af807b42a7210190a1a41b511
Alexander Chaikovsky <office2creative@gmail.com>
2023-01-14 23:21:52 +0200

    js added
    adding some js code to check mobile/desktop.

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,31 @@
 import { isMobile } from "./functions.js";
 // Підключення списку активних модулів
 import { flsModules } from "./modules.js";
+
+
+// Перевірка на мобільний пристрій
+// Повертає true, якщо пристрій мобільний
+// Повертає false, якщо пристрій десктопний
+function isItMobile() {
+    if (window.innerWidth <= 768) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+// поміняти клас на "_icon_logoMin" для мобільних пристроїв, або на "_icon_logo" для десктопу та планшетів 
+function logoChanger() {
+    if (isItMobile()) {
+        document.querySelector(".header__logo").classList.toggle("_icon_logoMin");
+      } else {
+        document.querySelector(".header__logo").classList.toggle("_icon_logo");
+      }
+      
+}
+
+logoChanger();
+
+
+
